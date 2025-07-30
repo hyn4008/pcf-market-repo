d6f0eddcb48ab6719de20f1614bfd0671a153c51
hwangy1 <yuna.hwang@hva.nl>
2025-07-30 14:04:08 +0900

    [INIT] 프로젝트 생성

---
+++ src/main/java/com/project/pcf_market/PcfMarketApplication.java
@@ -0,0 +1,13 @@
+package com.project.pcf_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcfMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcfMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/pcf_market/PcfMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pcf_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcfMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
